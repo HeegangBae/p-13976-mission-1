2080eca990a9257be5f6c5c8f8a83863738ddedd
heigangbae <j56233145@gmail.com>
2025-08-11 15:37:49 +0900

    feat : 1단게

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package com.back
+
+fun main() {
+    println("== 명언 앱 ==")
+    while(true){
+        print("명령) ")
+        val input = readlnOrNull()!!.trim()
+        if(input == "종료"){
+            break
+        }
+    }
+}
